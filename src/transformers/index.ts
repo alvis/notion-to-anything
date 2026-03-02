b57535be0d97e8ade407d140f183ad88d8656a67
Alvis HT Tang <alvis@hilbert.space>
2026-03-02 21:35:21 +0000

    feat(markdown): add markdown page transformer

---
+++ src/transformers/index.ts
@@ -0,0 +1,5 @@
+export { json } from './json';
+export { markdown } from './markdown';
+export { createBlockTransformer } from './create-block-transformer';
+
+export type { JsonBlock, JsonPage } from './json';
