ceb1bd3313ccb8e7680ab625e0b52a23fe1bb22f
Alvis HT Tang <alvis@hilbert.space>
2026-04-09 11:13:52 +0100

    feat(core): add concurrency primitives

---
+++ packages/core/src/index.ts
@@ -0,0 +1,6 @@
+export {
+  DEFAULT_CONCURRENCY,
+  mapWithConcurrency,
+  MAX_PAGE_SIZE,
+  resolveConcurrency,
+} from './traversal/index.ts';
---
+++ packages/core/src/traversal/index.ts
@@ -0,0 +1,2 @@
+export { mapWithConcurrency, resolveConcurrency } from './concurrency.ts';
+export { DEFAULT_CONCURRENCY, MAX_PAGE_SIZE } from './constants.ts';
